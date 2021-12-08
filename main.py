423c720deaa69ae11a24282f9b86714d4a9e649d
loann <loanloannguyen0038@gmail.com>
2021-12-09 00:37:49 +0700

    test

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import app as app
 import uvicorn
-from fastapi import Depends, FastAPI
-from fastapi import FastAPI, Path, Body, Depends
-from sqlalchemy.orm import Session, sessionmaker
-from starlette.requests import Request
 
 from fastapi import FastAPI
 
@@ -13,7 +8,3 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
