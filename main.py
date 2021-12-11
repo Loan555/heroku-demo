34d49632cb229ea7e9bed8e5890e63171e217c0a
loann <loan.nguyen2@monstar-lab.com>
2021-12-11 21:07:23 +0700

    test api

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def testConnectDB():
 
 @app.get("/")
 async def root():
-    return str(testConnectDB())
+    return {"messenger": "Hello wold"}
 
 
 @app.get("/test")
@@ -34,6 +34,5 @@ async def test():
         result.append(str(x))
     return {"result": result}
 
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
